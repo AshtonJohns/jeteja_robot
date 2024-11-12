1121a4084fc82092f90b89e5773fb6e6deaaf82e
= <=>
2024-11-12 12:33:07 -0600

    Changed for variable speed control

--- scripts/test_hardware.py
+++ scripts/test_hardware.py
@@ -48,27 +48,29 @@ def encode_dutycylce(ax_val_st, ax_val_th, params):
     STEERING_AXIS = params['steering_joy_axis']
     STEERING_CENTER = params['steering_center']
     STEERING_RANGE = params['steering_range']
-    THROTTLE_AXIS = params['throttle_joy_axis']
     THROTTLE_STALL = params['throttle_stall']
     THROTTLE_FWD_RANGE = params['throttle_fwd_range']
     THROTTLE_REV_RANGE = params['throttle_rev_range']
-    THROTTLE_LIMIT = params['throttle_limit']
-    RECORD_BUTTON = params['record_btn']
-    STOP_BUTTON = params['stop_btn']
-    
-    # Calculate steering and throttle value
+
+    # Calculate steering and throttle values
     act_st = -ax_val_st
     act_th = -ax_val_th  # throttle action: -1: max forward, 1: max backward
 
     # Encode steering value to duty cycle in nanoseconds
     duty_st = STEERING_CENTER - STEERING_RANGE + int(STEERING_RANGE * (act_st + 1))
-    # Encode throttle value to duty cycle in nanoseconds
+
+    # Encode throttle value with refined variable speed control
     if act_th > 0:
-        duty_th = THROTTLE_STALL + int(THROTTLE_FWD_RANGE * min(act_th, THROTTLE_LIMIT))
+        # Forward motion
+        duty_th = THROTTLE_STALL + int((THROTTLE_FWD_RANGE - THROTTLE_STALL) * act_th)
     elif act_th < 0:
-        duty_th = THROTTLE_STALL + int(THROTTLE_REV_RANGE * max(act_th, -THROTTLE_LIMIT))
+        # Reverse motion
+        duty_th = THROTTLE_STALL - int((THROTTLE_STALL - THROTTLE_REV_RANGE) * abs(act_th))
     else:
+        # No throttle
         duty_th = THROTTLE_STALL
+
+    # Encode to message format
     duty_st = round(duty_st, 2)
     duty_th = round(duty_th, 2)
     msg = encode(duty_st, duty_th)
