a2f95949fec6d9a2221565260e9a8d07579f1015
ashton <ashtonmjohns@gmail.com>
2024-11-12 19:21:12 -0600

    fix build

--- src/data_processing/setup.py
+++ src/data_processing/setup.py
@@ -7,9 +7,10 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/collect_data_launch.py']),
+        # ('share/' + package_name + '/data_processing', ['data_processing/extract_bags.py'])
     ],
     install_requires=[
         'setuptools',         # Core dependency for ROS 2 packages
