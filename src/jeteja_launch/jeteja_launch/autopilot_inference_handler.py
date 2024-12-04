227b5542b89cfb7ed1b19957335a6b780b2ef54a
= <=>
2024-12-04 01:07:23 -0600

    concat to bigger class, less ideal

--- src/jeteja_launch/jeteja_launch/autopilot_inference_handler.py
+++ src/jeteja_launch/jeteja_launch/autopilot_inference_handler.py
@@ -1,56 +1,83 @@
 import traceback
 import rclpy
 import scripts.postprocessing as postprocessing
+from scripts.preprocessing import ImageToRosMsg
 from rclpy.node import Node
-from jeteja_launch_msgs.msg import PwmSignals, PreprocessedImage
-from scripts.preprocessing import image_msg_to_numpy
+from sensor_msgs.msg import Image
+from jeteja_launch_msgs.msg import PreprocessedImage, PwmSignals
 from scripts.model_inference_handler import TensorRTInference
+
 
 class AutopilotInferenceHandler(Node):
     def __init__(self):
         super().__init__('autopilot_inference_handler')
 
-        self.image_sub = self.create_subscription(PreprocessedImage, '/autopilot/preprocessed_images', self.image_callback, 10)
+        self.image_exec = ImageToRosMsg()
 
-        self.pwm_pub = self.create_publisher(PwmSignals, '/pwm_signals', 10)
+        # Subscriptions to raw camera topics
+        self.color_sub = self.create_subscription(
+            Image, '/camera/camera/color/image_raw', self.color_callback, 10)
+        self.depth_sub = self.create_subscription(
+            Image, '/camera/camera/depth/image_rect_raw', self.depth_callback, 10)
 
+        # Publisher for combined preprocessed images
+        self.preprocessed_pub = self.create_publisher(
+            PreprocessedImage, '/autopilot/preprocessed_images', 10)
+        
+        # Model for inference
         self.trt_infer = TensorRTInference()
 
+        # PWM publisher
+        self.pwm_pub = self.create_publisher(PwmSignals, '/pwm_signals', 10)
+
+        # Internal storage for synchronized processing
         self.color_image = None
         self.depth_image = None
 
-    def image_callback(self, msg):
-        color_image = msg.color_image
-        depth_image = msg.depth_image
-        # self.get_logger().info(color_image)
-        # self.get_logger().info(depth_image)
-        # self.get_logger().info(color_image.encoding)
-        # self.get_logger().info(depth_image.encoding)
-        self.color_image = image_msg_to_numpy(color_image)
-        self.depth_image = image_msg_to_numpy(depth_image)
-        self.run_inference()
+    def color_callback(self, msg):
+        # Convert ROS Image message to NumPy array
+        self.color_image = self.image_exec.bridge_imgmsg_to_cv2(msg, is_color=True)
+        self.process_and_publish()
 
-    def run_inference(self):
+    def depth_callback(self, msg):
+        # Convert ROS Image message to NumPy array
+        self.depth_image = self.image_exec.bridge_imgmsg_to_cv2(msg, is_depth=True)
+        self.process_and_publish()
+
+    def process_and_publish(self):
         if self.color_image is not None and self.depth_image is not None:
-            # self.get_logger().info(self.color_image)
-            # self.get_logger().info(self.depth_image)
+            # Preprocess color and depth images
+            color_image = self.image_exec.preprocess(self.color_image,color=True)
+            depth_image = self.image_exec.preprocess(self.depth_image,depth=True)
 
-            outputs = self.trt_infer.infer(self.color_image, self.depth_image)
+            # Infer images
+            outputs = self.trt_infer.infer(color_image, depth_image)
+            motor_pwm, steering_pwm = postprocessing.denormalize_pwm(outputs)
 
-            self.get_logger().info(str(outputs[0][0][0]))
-            self.get_logger().info(str(outputs[1][0][0]))
+            # DEBUG
+            # self.get_logger().info(str(outputs[0][0][0]))
+            # self.get_logger().info(str(outputs[1][0][0]))
 
-            # motor_pwm, steering_pwm = postprocessing.denormalize_pwm(outputs)
+            # Publish PWM values
+            pwm_msg = PwmSignals()
+            pwm_msg.stamp = self.get_clock().now().to_msg()
+            pwm_msg.motor_pwm = motor_pwm
+            pwm_msg.steering_pwm = steering_pwm
+            self.pwm_pub.publish(pwm_msg)
 
+            self.color_image = None
+            self.depth_image = None
 
-            # # Publish PWM values
-            # pwm_msg = PwmSignals()
-            # pwm_msg.stamp = self.get_clock().now().to_msg()
-            # pwm_msg.motor_pwm = motor_pwm
-            # pwm_msg.steering_pwm = steering_pwm
+            # TODO ROS topic to publish image data
+            # # Convert back to ROS Image messages
+            # color_msg = self.image_exec.convert_image_array_to_ros_image_msg(color_image,color=True)
+            # depth_msg = self.image_exec.convert_image_array_to_ros_image_msg(depth_image,depth=True)
 
-            # self.pwm_pub.publish(pwm_msg)
-
+            # # Create and publish PreprocessedImage message
+            # preprocessed_msg = PreprocessedImage()
+            # preprocessed_msg.color_image = color_msg
+            # preprocessed_msg.depth_image = depth_msg
+            # self.preprocessed_pub.publish(preprocessed_msg)
 
 def main(args=None):
     rclpy.init(args=args)
