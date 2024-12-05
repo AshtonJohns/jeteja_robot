782e87d89e387a6477487d26b42486400b2a3f45
= <=>
2024-12-05 00:37:48 -0600

    include rosbag splitter

--- src/jeteja_record/launch/collect_data_launch.py
+++ src/jeteja_record/launch/collect_data_launch.py
@@ -15,6 +15,12 @@ def generate_launch_description():
         'topics.yaml'
     )
 
+    rosbag_config = os.path.join(
+        get_package_share_directory('jeteja_record'),
+        'config',
+        'rosbag.yaml'
+    )
+
     # Get the workspace directory from an environment variable or default to the install space
     workspace_dir = os.getenv('ROS_WORKSPACE', os.path.abspath(os.path.join(
         get_package_share_directory('jeteja_launch'), '..', '..', '..', '..', # TODO is this a good practice? 
@@ -28,12 +34,24 @@ def generate_launch_description():
         topics_config = yaml.safe_load(file)
     topics = topics_config['topics']  # List of topics from YAML
 
+    # Load rosbag configurations
+    with open(rosbag_config, 'r') as file:
+        rosbag_config = yaml.safe_load(file)
+    split_size = rosbag_config['split_size']
+
     return LaunchDescription([
         # Declare output directory argument
         DeclareLaunchArgument(
             'output_dir',
             default_value=default_output_dir,
             description='Directory where the rosbag will be saved'
+        ),
+
+        # Declare split size for rosbag argument
+        DeclareLaunchArgument(
+            'split_size',
+            default_value=split_size,
+            description='Split rosbag once it has reached the size.'
         ),
         
         # Rosbag Manager Node
@@ -44,6 +62,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'topics': topics,  # Pass topics as a parameter
+                'split_size': LaunchConfiguration('split_size'),
                 'output_dir': LaunchConfiguration('output_dir')  # Pass output directory as a parameter
             }]
         )
--- src/jeteja_record/setup.py
+++ src/jeteja_record/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/collect_data_launch.py']),
+        ('share/' + package_name + '/config', ['config/rosbag.yaml']), 
         ('share/' + package_name + '/config', ['config/topics.yaml']), 
     ],
     install_requires=[
